abbd6510c361e4abddd83e13e0f2411238a399d7
ChangLiuuu <spiritchang@hotmail.com>
2017-04-09 11:11:57 -0700

    boom~

--- Arrays and Strings/isUnique.java
+++ Arrays and Strings/isUnique.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
 
-public class isUnique {
+public class IsUnique {
+	
 	public static boolean solution(String s) {
 		boolean[] array = new boolean[256];
 		for (int i = 0; i < s.length(); i++) {
@@ -23,9 +25,22 @@ public class isUnique {
 		return true;
 	}
 	
+	// sort then check neighboring characters
+	public static boolean solution3(String s) {
+		char[] array = s.toCharArray();
+		Arrays.sort(array);
+		for (int i = 1; i < s.length(); i++) {
+			if (array[i] == array[i - 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(solution("afsdf"));
 		System.out.println(solution2("asdf"));
+		System.out.println(solution3("asdfa"));
 		
 	}	
 }
