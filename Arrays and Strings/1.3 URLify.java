387c3eee1a009dc587eba5633acc80dfac520c77
ChangLiuuu <spiritchang@hotmail.com>
2017-04-10 11:37:36 -0700

    1.3

--- Arrays and Strings/1.3 URLify.java
+++ Arrays and Strings/1.3 URLify.java
@@ -18,7 +18,31 @@ public class URLify {
 		return sb.toString();
 	}
 	
+	public static String repleaceSpaces2(String str, int trueLength) {
+		char[] array = str.toCharArray();
+		int spaceCount = 0;
+		for (int i = 0; i < trueLength; i++) {
+			if (array[i] == ' ') {
+				spaceCount++;
+			}
+		}
+		char[] res = new char[array.length + spaceCount * 2];
+		int index = res.length - 1;
+		for (int i = trueLength - 1; i >= 0; i--) {
+			if (array[i] == ' ') {
+				res[index] = '0';
+				res[index - 1] = '2';
+				res[index - 2] = '%';
+				index = index - 3;
+			} else {
+				res[index] = array[i];
+				index--;
+			}
+		}
+		return new String(res);
+	}
 	public static void main(String[] args) {
 		System.out.println(replaceSpaces("Mr S M   ", 7));
+		System.out.println(repleaceSpaces2("Mr S M   ", 6));
 	}	
 }
