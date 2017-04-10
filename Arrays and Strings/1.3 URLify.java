975175fff5ced9c638ade13610e2df89a1c7a20b
ChangLiuuu <spiritchang@hotmail.com>
2017-04-10 11:43:38 -0700

    solutions
    1.3

--- Arrays and Strings/1.3 URLify.java
+++ Arrays and Strings/1.3 URLify.java
@@ -4,9 +4,7 @@ public class URLify {
 	public static String replaceSpaces(String str, int trueLength) {
 		char[] array = str.toCharArray();
 		StringBuilder sb = new StringBuilder();
-		if (trueLength < array.length) {
-			array[trueLength] = '\0';
-		} 
+
 
 		for (int i = 0; i < trueLength; i++) {
 			if (array[i] == ' ') {
