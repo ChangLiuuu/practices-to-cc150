89421ec408ad37d45dd4cb4afa151e6e1fdf84b3
ChangLiuuu <spiritchang@hotmail.com>
2017-04-12 17:39:35 -0700

    1.4

--- 1 Arrays and Strings/1.4 PalindromePermutation.java
+++ 1 Arrays and Strings/1.4 PalindromePermutation.java
@@ -40,6 +40,7 @@ public class PalindromePermutation {
 		}
 		return oddCount <= 1;
 	}
+    
 	public static void main(String[] args) {
 		System.out.println(solution("googleela"));
 		System.out.println(solution2("googleelab"));
